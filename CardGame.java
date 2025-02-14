840c20871a8de9aa9e1749dc8aaae491fc9c830c
RyanDiaz25 <qsbb8275@gmail.com>
2025-02-13 18:02:51 -0800

    Update CardGame.java

--- CardGame.java
+++ CardGame.java
@@ -1,17 +1,22 @@
-package cardGame;
+//Group Members: Ryan, Mayerly, Raymond, Hector
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+//import javax.smartcardio.Card; <-- Broke the code, not sure why
 
 public class CardGame {
 
 	private static ArrayList<Card> deckOfCards = new ArrayList<Card>();
 	private static ArrayList<Card> playerCards = new ArrayList<Card>();
 
+    
 
 	public static void main(String[] args) {
+
 
 		Scanner input = null;
 		try {
@@ -23,28 +28,25 @@ public class CardGame {
 		}
 
 		while(input.hasNext()) {
-			String[] fields  = input.nextLine().split(",");
-			//	public Card(String cardSuit, String cardName, int cardValue, String cardPicture) {
-			Card newCard = new Card(fields[0], fields[1].trim(),
-					Integer.parseInt(fields[2].trim()), fields[3]);
+			ArrayList<String> fields = new ArrayList<>(Arrays.asList(input.nextLine().split(","))); //Changed to ArrayList
+			Card newCard = new Card(fields.get(0), fields.get(1).trim(), //now uses .get to access ArrayList
+					Integer.parseInt(fields.get(2).trim()), fields.get(3)); //now uses .get to access ArrayList
 			deckOfCards.add(newCard);	
 		}
 
 		shuffle();
 
-		//for(Card c: deckOfCards)
-			//System.out.println(c);
 
-		//deal the player 5 cards
-		for(int i = 0; i < 4; i++) {
+		//updated to deal the player 7 cards, alter to liking
+		for(int i = 0; i < 7; i++) {
 			playerCards.add(deckOfCards.remove(i));
 		}
 		
-		System.out.println("players cards");
+		System.out.println("Players Cards:");
 		for(Card c: playerCards)
 			System.out.println(c);
 
-		System.out.println("pairs is " + checkFor2Kind());
+		System.out.println("pairs is " + checkFor2Kind()); //calls pair method to detertimne true or false
 
 	}//end main
 
@@ -54,7 +56,6 @@ public class CardGame {
 		for (int i = 0; i < deckOfCards.size(); i++) {
 			int index = (int) (Math.random()*deckOfCards.size());
 			Card c = deckOfCards.remove(index);
-			//System.out.println("c is " + c + ", index is " + index);
 			deckOfCards.add(c);
 		}
 	}
@@ -69,8 +70,7 @@ public class CardGame {
 			
 			for(int j = i+1; j < playerCards.size(); j++) {
 				next = playerCards.get(j);
-				//System.out.println(" comparing " + current);
-				//System.out.println(" to " + next);
+				
 				if(current.equals(next))
 					count++;
 			}//end of inner for
@@ -79,5 +79,9 @@ public class CardGame {
 
 		}//end outer for
 		return false;
+
+		
 	}
+	
 }//end class
+
